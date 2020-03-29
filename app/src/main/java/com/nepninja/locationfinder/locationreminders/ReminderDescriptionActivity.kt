ef45f82989422bba0dd222eae3b668a176cdd94c
rajesh <rajesh.k.khadka@gmail.com>
2020-03-29 16:53:57 +0545

    display the detail of reminder

--- app/src/main/java/com/nepninja/locationfinder/locationreminders/ReminderDescriptionActivity.kt
+++ app/src/main/java/com/nepninja/locationfinder/locationreminders/ReminderDescriptionActivity.kt
@@ -5,6 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.nepninja.locationfinder.R
 import com.nepninja.locationfinder.databinding.ActivityReminderDescriptionBinding
 import com.nepninja.locationfinder.locationreminders.reminderslist.ReminderDataItem
@@ -32,6 +37,29 @@ class ReminderDescriptionActivity : AppCompatActivity() {
             this,
             R.layout.activity_reminder_description
         )
-//        TODO: Add the implementation of the reminder details
+
+        val reminderDataItem =
+            intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem
+        binding.reminderDataItem = reminderDataItem
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_reminder_description) as SupportMapFragment
+        mapFragment.getMapAsync { map ->
+            val latLng = LatLng(reminderDataItem.latitude!!, reminderDataItem.longitude!!)
+            map.addMarker(
+                MarkerOptions().position(
+                    LatLng(
+                        reminderDataItem.latitude!!,
+                        reminderDataItem.longitude!!
+                    )
+                )
+            )
+            val cameraPosition = CameraPosition
+                .Builder()
+                .target(latLng)
+                .zoom(13f)
+                .build()
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        }
     }
 }
