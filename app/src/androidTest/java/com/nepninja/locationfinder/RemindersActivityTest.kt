978574c37915dd98b58328ccc90d8eb26e587dc7
rajesh <rajesh.k.khadka@gmail.com>
2020-04-03 22:35:59 +0545

    add test case to list reminders

--- app/src/androidTest/java/com/nepninja/locationfinder/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/nepninja/locationfinder/RemindersActivityTest.kt
@@ -1,16 +1,29 @@
 package com.nepninja.locationfinder
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.nepninja.locationfinder.data.ReminderDataSource
+import com.nepninja.locationfinder.data.dto.ReminderDTO
 import com.nepninja.locationfinder.data.local.LocalDB
 import com.nepninja.locationfinder.data.local.RemindersLocalRepository
+import com.nepninja.locationfinder.locationreminders.RemindersActivity
 import com.nepninja.locationfinder.reminderslist.RemindersListViewModel
 import com.nepninja.locationfinder.savereminder.SaveReminderViewModel
+import com.nepninja.locationfinder.util.DataBindingIdlingResource
+import com.nepninja.locationfinder.util.monitorActivity
+import com.nepninja.locationfinder.utils.EspressoIdlingResource
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -27,6 +40,8 @@ class RemindersActivityTest :
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -63,6 +78,46 @@ class RemindersActivityTest :
         runBlocking {
             repository.deleteAllReminders()
         }
+    }
+
+    /**
+     * Idling resources tell Espresso that the app is idle or busy. This is needed when operations
+     * are not scheduled in the main Looper (for example when executed on a different thread).
+     */
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    /**
+     * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Test
+    fun getReminders() = runBlocking {
+        val reminder = ReminderDTO(
+            "Birthday Reminder"
+            , "Grandma Birthday I need to buy cake"
+            , "Big complex freak street"
+            , 24.00
+            , 48.00000
+        )
+
+        repository.saveReminder(reminder)
+
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withText("Birthday Reminder")).check(matches(isDisplayed()))
+        onView(withText("Grandma Birthday I need to buy cake")).check(matches(isDisplayed()))
+
+        activityScenario.close()
     }
 
 
