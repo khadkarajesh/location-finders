ed34e89356e487059de15a3b0bdf60255afc7944
rajesh <rajesh.k.khadka@gmail.com>
2020-04-04 09:48:28 +0545

    add test case for empty data

--- app/src/androidTest/java/com/nepninja/locationfinder/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/nepninja/locationfinder/reminderslist/ReminderListFragmentTest.kt
@@ -1,12 +1,11 @@
 package com.nepninja.locationfinder.reminderslist
 
+import android.app.Application
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
-import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
@@ -15,59 +14,78 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.nepninja.locationfinder.R
-import com.nepninja.locationfinder.base.BaseViewModel
 import com.nepninja.locationfinder.data.ReminderDataSource
+import com.nepninja.locationfinder.data.local.LocalDB
 import com.nepninja.locationfinder.data.local.RemindersLocalRepository
-import com.nepninja.locationfinder.data.local.RemindersLocalRepositoryTest
+import com.nepninja.locationfinder.savereminder.SaveReminderViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Assert.assertTrue
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.context.loadKoinModules
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
-import org.mockito.ArgumentMatchers
-import org.mockito.Mockito
-import org.mockito.Mockito.*
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
-//@RunWith(AndroidJUnit4::class)
-@ExperimentalCoroutinesApi
-//UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+class ReminderListFragmentTest : AutoCloseKoinTest() {
 //    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
 //    TODO: add testing for the error messages.
 
-    lateinit var mockViewModel: RemindersListViewModel
-    private lateinit var repository: RemindersLocalRepositoryTest
+    private lateinit var repository: ReminderDataSource
+    private lateinit var appContext: Application
 
     @Before
-    fun setUp() {
-        mockViewModel = mock(RemindersListViewModel::class.java)
-        loadKoinModules(module {
-            mockViewModel
-        })
+    fun init() {
+        stopKoin()//stop the original app koin
+        appContext = ApplicationProvider.getApplicationContext()
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { LocalDB.createRemindersDao(appContext) }
+        }
+        //declare a new koin module
+        startKoin {
+            modules(listOf(myModule))
+        }
+        //Get our real repository
+        repository = get()
 
-        repository = RemindersLocalRepositoryTest()
+        //clear the data to start fresh
+        runBlocking {
+            repository.deleteAllReminders()
+        }
     }
 
 
     @Test
-    fun shouldShowError() = runBlocking {
+    fun showNoData_onEmptyData() = runBlockingTest {
         val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
         val navController = mock(NavController::class.java)
 
         scenario.onFragment {
             Navigation.setViewNavController(it.view!!, navController)
         }
-
-//        repository.getReminders()
 
         onView(withId(R.id.noDataTextView)).check(ViewAssertions.matches(isDisplayed()))
     }
@@ -87,11 +105,5 @@ class ReminderListFragmentTest {
         verify(navController).navigate(
             ReminderListFragmentDirections.toSaveReminder()
         )
-    }
-
-
-    @After
-    fun cleanUp() {
-        stopKoin()
     }
 }
